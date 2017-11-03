ba640935ae0162a67a0bb0ab5ac8f9660609a3d1
pThiel157 <thiel157@gmail.com>
2017-11-02 22:59:08 -0400

    Added classes.py, test-server-(local).py

---
+++ classes.py
@@ -0,0 +1,29 @@
+class Post:
+	
+	def __init__(self, user, item):
+	
+		self.resolved = False
+	
+
+	
+
+	
+class FoundPost(Post):  # inherits post
+
+
+
+class LostPost(Post):  # inherits post
+
+
+
+class Item:
+	
+
+
+class User:
+	
+	
+
+class LostAndFoundBin:
+	
+	
