d53e6a32b754b33c12534bdc52ee91a951ef630d
pThiel157 <thiel157@gmail.com>
2017-11-14 21:03:43 -0500

    Prettied up webpage, formatted it to accept input for new posts and prepared it to connect to a search algorithm to search for stored posts

--- server/my_jquery_functions.js
+++ server/my_jquery_functions.js
@@ -1,3 +1,8 @@
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+var $ = document.getElementsByTagName('head')[0].appendChild(script);
+
 $(document).ready(function(){
     $("button").click(function(){
         $("form").hide();
--- server/server.js
+++ server/server.js
@@ -8,14 +8,14 @@ var util = require('util');
 var server = http.createServer(function (req, res) {
 	
 	if (req.method.toLowerCase() == 'get') {
-		displayForm(res);
+		displayPage(res);
 	} else if (req.method.toLowerCase() == 'post') {
 		processForm(req, res);
 	}
 });
 
 
-function displayForm(res) {
+function displayPage(res) {
     fs.readFile('form.html', function (err, data) {
         res.writeHead(200, {
             'Content-Type': 'text/html',
@@ -27,29 +27,40 @@ function displayForm(res) {
 
 
 function processForm(req, res) {
+	
 	var form = new formidable.IncomingForm();
 	
 	form.parse(req, function (err, fields, files) {
-        // here we write the info from the form into a file
-		fs.appendFile("DATA/user-input-data.json", util.inspect(fields), function(err) {
-			if(err) {
-				return console.log(err)
-			}
-		});
+		if (util.inspect(fields).username == "" || util.inspect(fields).item_name == "") {  // ***MAKE THIS LINE WORK! MAYBE UTIL.INSPECT RETURNS A STRING?***
+			// ***TELL THE USER THEY NEED A USERNAME AND ITEM NAME***
+		}
+		else {
+			// here we write the info from the form into a file
+			fs.appendFile("DATA/user-input-data.json", util.inspect(fields), function(err) {
+				if(err) {
+					return console.log(err)
+				}
+			});
+		}
+		displayPage(res); // display the main page again
+		
+		// ***CALL TO JQUERY TO ADD "SUBMISSION ADDED" TEXT TO HTML***
+		
+		
 		
 		// now we prepare to read from the data file and write it on the webpage
-        res.writeHead(200, {
-            'content-type': 'text/plain'
-        });
-        res.write('loaded data from input file:\n\n');
-		var content;
-		fs.readFile("DATA/user-input-data.json", 'utf8', function read(err, data) {
-			if (err) {
-				throw err;
-			}
-			console.log(data);
-			res.end(data);
-		});
+        // res.writeHead(200, {
+            // 'content-type': 'text/plain'
+        // });
+        // res.write('loaded data from input file:\n\n');
+
+		// fs.readFile("DATA/user-input-data.json", 'utf8', function read(err, data) {
+			// if (err) {
+				// throw err;
+			// }
+			// console.log(data);
+			// res.end(data);
+		// });
     });
 }
 
