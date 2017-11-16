5f9d6efba620e76d90a459d22331c819c599c833
TMurphy24 <taymurph4@gmail.com>
2017-11-15 22:56:35 -0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -26,4 +26,30 @@ class User:
 
 class LostAndFoundBin:
 	
-	
+//
+//Reads and parses out the data for searchability    
+    JSONArray a = (JSONArray) parser.parse(new FileReader("My desired JSON"));
+    
+    for (Object o : a)
+  {
+    JSONObject USER = (JSONObject) o;
+
+    String name = (String) USER.get("name");
+    System.out.println(name);
+
+    String ITEM = (String) USER.get("item");
+    System.out.println(ITEM);
+
+    
+ public class POST 
+ public static void main(String[] args) { 
+}: 
+ 
+var LOSTPOST:
+
+var FOUNDPOST:
+    
+var ITEM: 
+
+var USER:
+
